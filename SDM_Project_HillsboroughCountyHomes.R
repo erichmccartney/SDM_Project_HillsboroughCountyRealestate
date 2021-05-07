b7c14f16df4273e478a92764c9f4ebac650d16eb
cbertoli28 <cbertoli28@gmail.com>
2021-05-06 22:18:10 -0400

    Linear, Fixed, OLS

--- SDM_Project_HillsboroughCountyHomes.R
+++ SDM_Project_HillsboroughCountyHomes.R
@@ -1,11 +1,43 @@
 #' SDM Project: Hillsborough County Real Estate
 
-setwd("~/GitHub/R/DataSets")
 df <- read.csv("HillsboroughCountyData.csv")
 str(df)
-view(df)
+View(df)
 
+hist(df$YearsSinceTurnover)
+hist(log(df$YearsSinceTurnover))
+  
+#DensityPlot 
+library(lattice)
+densityplot(~YearsSinceTurnover | Avg_GradePoint2019, data=df)
 
-  
-  
-#' Feature engineering/Data Preprocessing
+#Linear Regression
+#Summary: 
+linearMod <- lm(YearsSinceTurnover ~ Avg_GradePoint2019, data=df) 
+print(linearMod)
+
+#Pooled OLS Model 
+ols <- lm(YearsSinceTurnover ~ Avg_GradePoint2019*LastSalePrice, data=df)
+summary(ols)
+
+#Fixed Effects Model 
+fe <- lm(YearsSinceTurnover ~ Avg_GradePoint2019*LastSalePrice + Avg_GradePoint2019*SchoolZipCodeGroup + as.factor(Neighborhood), data=df)
+summary(fe)
+
+library(stargazer)
+options(max.print = 60000)
+stargazer(linearMod, ols, fe, type="text", single.row=TRUE)
+
+#Test for Assumptions 
+hist(ols$res)
+ols$fit
+
+#ResidualPlot 
+plot(fe$res ~ fe$fit)  
+
+##QQPlot 
+qqnorm(fe$res)
+qqline(fe$res, col="red")
+
+#Shapiro-Wilk's Test inconclusive sample size must be between 3 and 5000 
+shapiro.test(fe$res)  
